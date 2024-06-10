9055e4f5e867044d4e38f2b91b75f2c97fac25e4
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2024-06-09 20:22:47 -0500

    Refactor: Abstract read json file logic into a function

--- tests/parser_test.go
+++ tests/parser_test.go
@@ -10,27 +10,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestParserPokemon(t *testing.T) {
+func readJsonFile[T any](t *testing.T, path string) T {
+  c := require.New(t)
+  body, err := os.ReadFile(path)
+  c.NoError(err)
+
+  var response T
+  err = json.Unmarshal(body, &response)
+  c.NoError(err)
+
+  return response
+}
+
+func TestParserPokemonSuccess(t *testing.T) {
 	c := require.New(t)
 
-	body, err := os.ReadFile("../utils/samples/pokeapi_response.json")
-	c.NoError(err)
-
-	var response models.PokeApiPokemonResponse
-
-	err = json.Unmarshal(body, &response)
-	c.NoError(err)
+  response := readJsonFile[models.PokeApiPokemonResponse](t, "../utils/samples/pokeapi_response.json")
 
 	pokemon, err := utils.ParsePokemon(response)
   c.NoError(err)
 
-	body, err = os.ReadFile("../utils/samples/api_response.json")
-	c.NoError(err)
-
-	var expectedPokemon *models.Pokemon
-
-	err = json.Unmarshal(body, &expectedPokemon)
-	c.NoError(err)
+  expectedPokemon := readJsonFile[*models.Pokemon](t, "../utils/samples/api_response.json")
 
   c.Equal(expectedPokemon, pokemon)
 }
+
