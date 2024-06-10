58d21b7f6d863c961c40074efe3d18a84ef94c5b
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2024-06-09 23:14:26 -0500

    Moking HTTP clients

--- controllers/pokeapi_controller.go
+++ controllers/pokeapi_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -12,7 +13,59 @@ import (
 	"github.com/nicrodriguezval/unit-test/utils"
 )
 
-// respondwithJSON write json response format
+var (
+  ErrPokemonNotFound = errors.New("pokemon not found")
+  ErrPokeapiFailure = errors.New("unexpected response from pokeapi")
+)
+
+func GetPokemon(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+  apiPokemon, err := GetPokemonFromPokeapi(id)
+  if errors.Is(err, ErrPokemonNotFound) {
+    respondwithJSON(w, http.StatusNotFound, fmt.Sprintf("pokemon with id %s not found", id))
+  }
+  if err != nil {
+    respondwithJSON(w, http.StatusInternalServerError, fmt.Sprintf("error while calling pokeapi: %s", err.Error()))
+  }
+
+	parsedPokemon, err := utils.ParsePokemon(apiPokemon)
+	if err != nil {
+		respondwithJSON(w, http.StatusInternalServerError, fmt.Sprintf("error found: %s", err.Error()))
+	}
+
+	respondwithJSON(w, http.StatusOK, parsedPokemon)
+}
+
+func GetPokemonFromPokeapi(id string) (*models.PokeapiPokemonResponse, error) {
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	response, err := http.Get(request)
+	if err != nil {
+    return nil, err
+	}
+
+  if response.StatusCode == http.StatusNotFound {
+    return nil, ErrPokemonNotFound
+  }
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+    return nil, err
+	}
+
+  defer response.Body.Close()
+
+	var apiPokemon *models.PokeapiPokemonResponse
+
+	err = json.Unmarshal(body, &apiPokemon)
+	if err != nil {
+    return nil, err
+	}
+
+  return apiPokemon, nil
+}
+
 func respondwithJSON(w http.ResponseWriter, code int, payload any) {
 	response, err := json.Marshal(payload)
 	if err != nil {
@@ -26,34 +79,4 @@ func respondwithJSON(w http.ResponseWriter, code int, payload any) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func GetPokemon(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
-
-	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
-
-	response, err := http.Get(request)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var apiPokemon models.PokeApiPokemonResponse
-
-	err = json.Unmarshal(body, &apiPokemon)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	parsedPokemon, err := utils.ParsePokemon(apiPokemon)
-	if err != nil {
-		respondwithJSON(w, http.StatusInternalServerError, fmt.Sprintf("error found: %s", err.Error()))
-	}
-
-	respondwithJSON(w, http.StatusOK, parsedPokemon)
 }
--- tests/pokeapi_controller_test.go
+++ tests/pokeapi_controller_test.go
@@ -1,8 +1,13 @@
 package tests
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 	"testing"
 
+	"github.com/jarcoal/httpmock"
 	controller "github.com/nicrodriguezval/unit-test/controllers"
 	"github.com/nicrodriguezval/unit-test/models"
 	"github.com/nicrodriguezval/unit-test/utils"
@@ -16,6 +21,31 @@ func TestGetPokemonFromPokeapiSuccess(t *testing.T) {
   c.NoError(err)
 
   expectedPokemon := utils.ReadJsonFile[*models.PokeapiPokemonResponse](t, utils.POKEAPI_RESPONSE_PATH)
+
+  c.Equal(expectedPokemon, pokemon)
+}
+
+func TestGetPokemonFromPokeapiWithMocksSuccess(t *testing.T) {
+  c := require.New(t)
+
+  httpmock.Activate()
+  defer httpmock.DeactivateAndReset()
+
+  body, err := os.ReadFile(utils.POKEAPI_RESPONSE_PATH)
+  c.NoError(err)
+
+  id := "bulbasaur"
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+  httpmock.RegisterResponder("GET", request, httpmock.NewStringResponder(http.StatusOK, string(body)))
+
+  pokemon, err := controller.GetPokemonFromPokeapi(id)
+  c.NoError(err)
+
+  var expectedPokemon *models.PokeapiPokemonResponse
+
+  err = json.Unmarshal(body, &expectedPokemon)
+  c.NoError(err)
 
   c.Equal(expectedPokemon, pokemon)
 }
